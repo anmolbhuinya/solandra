3d5900e4eac63f5a8b766e8f4577f5aa2b97165d
anmolbhuinya <anmolbhuinya@gmail.com>
2016-09-26 02:10:45 +0530

    Correcting scripts

--- scripts/cassandra_start.sh
+++ scripts/cassandra_start.sh
@@ -2,7 +2,7 @@ echo "Going from Scripts to Cassandra Bin Folder"
 cd ../apache-cassandra-3.5/bin
 
 echo "Starting cassandra"
-./cassandra start&
+./cassandra
 
 echo "Going back to Scripts Folder"
 cd ../../Scripts/
--- scripts/solr_configure.sh
+++ scripts/solr_configure.sh
@@ -5,7 +5,7 @@ echo "Creating folder rental inside solr server"
 mkdir rental
 
 echo "Copying conf folder containing schema.xml, solrconfig.xml and other files from Source folder"
-cp -r ../../../Source/solandra/conf .
+cp -r ../../../Source/solandra/solr/rental/conf rental/.
 
 echo "Going back to Scripts Folder"
 cd ../../../Scripts/
